a8631bdd8d7f0c6baaa68aa724256e75f48b7e97
Rajiv Vishwa <rajivvishwa@gmail.com>
2025-05-07 20:48:38 -0700

    Add pihole_api_dns.py and pihole_auth.py for Pi-hole API integration

---
+++ pihole_api_dns.py
@@ -0,0 +1,79 @@
+import requests
+import logging
+from dotenv import load_dotenv
+import pandas as pd
+import os
+import keyring
+from keyring.errors import PasswordDeleteError
+from pihole_auth import get_valid_token
+from pihole_auth import get_token_keyring
+import streamlit as st
+from rich.console import Console
+from rich.table import Table
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Constants
+KEYRING_SERVICE = "pihole-auth"
+pihole_server = None
+sid = None
+csrf = None
+
+def get_cnames():
+    """
+    Fetches CNAME records from the Pi-hole server.
+    """
+    url = f'https://{pihole_server}/api/config/dns/cnameRecords'
+
+    payload = {}
+    headers = {
+    "X-FTL-SID": sid,
+    "X-FTL-CSRF": csrf
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+    print(response.text)
+
+    records = response.json()["config"]["dns"]["cnameRecords"]
+
+    # Split each record into host and target
+    split_records = [rec.split(',') for rec in records]
+
+    # Create DataFrame
+    cnames_df = pd.DataFrame(split_records, columns=["host", "target"])
+
+    return cnames_df  
+
+def main():
+    """
+    Main function to execute the script.
+    """
+
+    global pihole_server
+    global sid
+    global csrf
+
+    # Load environment variables from .env file
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+    pihole_server = os.getenv("PIHOLE_SERVER")
+    pihole_password = os.getenv("PIHOLE_PASSWORD")
+
+    # Retrieve the token from the keyring
+    get_valid_token()
+    sid, csrf = get_token_keyring()
+
+    if not sid or not csrf:
+        logging.error("Failed to retrieve session ID or CSRF token from keyring.")
+        return
+
+    # Print the session ID and CSRF token
+    logging.info(f"Session ID: {sid}")
+    logging.info(f"CSRF Token: {csrf}")
+    # Fetch CNAME records
+    cnames_df = get_cnames()
+
+    st.dataframe(cnames_df)
+
+if __name__ == "__main__":
+   main()
---
+++ pihole_auth.py
@@ -0,0 +1,131 @@
+import requests
+import os
+import logging
+from dotenv import load_dotenv
+import pandas as pd
+import keyring
+from keyring.errors import PasswordDeleteError
+from rich.console import Console
+from rich.table import Table
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Constants
+KEYRING_SERVICE = "pihole-auth"
+
+def pihole_auth(pihole_server, pihole_password):
+    """
+    Authenticate with the Pi-hole server using the API key.
+    """
+    url = f'https://{pihole_server}/api/auth'
+    payload = {"password": pihole_password}
+
+    # Disable SSL warnings for self-signed certificates
+    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+    # Send POST request to authenticate
+    try:
+        response = requests.post(url, json=payload, verify=False)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        logging.info("Authentication successful.")
+        token = response.json()
+        sid = token['session']['sid']
+        if not sid:
+            logging.error("Failed to retrieve session ID.")
+            return None
+        else:
+            logging.info("Session ID retrieved successfully.")
+        return token
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Authentication failed: {e}")
+        return None
+
+def put_token_keyring(token):
+    """Store the token in the system keyring."""
+    sid = token['session']['sid']
+    csrf = token['session']['csrf']
+    try:
+        keyring.delete_password(KEYRING_SERVICE, "sid")
+        keyring.delete_password(KEYRING_SERVICE, "csrf")
+    except PasswordDeleteError:
+        pass  # Ignore if the password doesn't exist yet
+
+    keyring.set_password(KEYRING_SERVICE, "sid", sid)
+    keyring.set_password(KEYRING_SERVICE, "csrf", csrf)
+
+    # Store the token in the keyring
+    print("Token stored in keyring successfully.")
+
+def get_token_keyring():
+    """Retrieve the token from the system keyring."""
+    try:
+        sid = keyring.get_password(KEYRING_SERVICE, "sid")
+        csrf = keyring.get_password(KEYRING_SERVICE, "csrf")
+        return sid, csrf
+    except keyring.errors.KeyringError as e:
+        logging.error(f"Error retrieving token from keyring: {e}")
+        return None
+
+def get_valid_token():
+    """
+    Retrieve a valid token from the keyring or fetch a new one if not available.
+    """ 
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+    pihole_server = os.getenv("PIHOLE_SERVER")
+    pihole_password = os.getenv("PIHOLE_PASSWORD")
+
+    if not pihole_server or not pihole_password:
+        logging.error("Please set the PIHOLE_SERVER and PIHOLE_PASSWORD environment variables.")
+    else:
+        logging.info("Loaded environment variables successfully.")
+    sid, csrf = get_token_keyring()
+
+    if not sid or not csrf:
+        logging.error("Failed to retrieve session ID or CSRF token from keyring.")
+        token = pihole_auth(pihole_server, pihole_password)
+        if not token:
+            logging.error("Failed to authenticate with Pi-hole server.")
+            return
+        # Store the token in the keyring
+        put_token_keyring(token)
+    else:
+        logging.info("Checking if the token is valid...")
+        url = f'https://{pihole_server}/api/auth'
+        payload = {}
+        headers = {
+        "X-FTL-SID": sid,
+        "X-FTL-CSRF": csrf
+        }
+
+        # Disable SSL warnings for self-signed certificates
+        requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+        # Send POST request to authenticate
+        try:
+            response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+            token = response.json()
+            if token['session']['valid'] == True:
+                logging.info("Token in keyring is valid.")
+            else:
+                logging.error("Stored token is invalid, fetching a new one...")
+                token = pihole_auth(pihole_server, pihole_password)
+                if not token:
+                    logging.error("Failed to authenticate with Pi-hole server.")
+                    return
+                # Store the token in the keyring
+                put_token_keyring(token)
+                logging.info("Token stored in keyring successfully.")
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Authentication failed: {e}")
+            return None
+
+def main():
+    """
+    Main function to execute the script.
+    """
+
+    get_valid_token()
+
+if __name__ == "__main__":
+   main()
